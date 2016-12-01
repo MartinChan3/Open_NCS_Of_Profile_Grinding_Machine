43c8b59858d052691df8ed4c4e7d949ac689bf56
Martin Chan <bycy0801@sjtu.edu.cn>
2016-12-01 20:07:34 +0800

    第一个单线程版本
    增加了执行文件，暂时采取了TRANSFER_FILE的文件名；多线程模块需要加入；下位机监控模块

--- mainwindow.h
+++ mainwindow.h
@@ -8,7 +8,7 @@
 #include <QPalette>
 #include <QAxObject>
 #include <QLabel>
-
+#include <QDir>
 #include "trioactivex.h"
 
 namespace Ui {
@@ -33,15 +33,18 @@ private:
     QPalette Palette_Unpressed,Palette_Pressed,Palette_Unconnected,Palette_Connected;
     QLabel *Label_Connection_Status;
     int current_ButtonID_main,current_ButtonID_sub,current_ButtonID_sub2;
+    QDir dir_of_txt;
+    bool Connection_Status_of_Trio;
 
     void button_pressed(QPushButton *button);
     void button_unpressed(QPushButton *button);
     void clear_button_text(BUTTON_GROUP_TYPE);
 
-
     TrioPCLib::TrioPC *trio;
 
 private slots:
+    void errors_handled(int);
+
     void pressed_mainButtonGroup(int i);
     void pressed_subButtonGroup(int i);
     void pressed_sub2ButtonGroup(int i);
@@ -52,6 +55,16 @@ private slots:
     void txtfile_undo();
     void txtfile_grammar_check();
     void txtfile_send_to_trio();
+
+    void cB_Txt_Dir_Content(QString);
+    void cB_current_index_changed(QString);
+
+    void pB_Connection();
+
+signals:
+    void cB_Txt_Changed(QString);
+    void status_Changed(int);
+    void errors_in_runtime(int);
 
 };
 
